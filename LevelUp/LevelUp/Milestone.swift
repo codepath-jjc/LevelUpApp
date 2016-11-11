0294bc6a8af05a9f706504662978ccc509fd078c
jescriba <escribirajoshua@gmail.com>
2016-11-10 18:35:34 -0800

    Reinstalled pods and added milestone model

--- LevelUp/LevelUp/Milestone.swift
+++ LevelUp/LevelUp/Milestone.swift
@@ -1,27 +1,27 @@
 //
-//  Quest.swift
+//  Milestone.swift
 //  LevelUp
 //
-//  Created by jason on 11/9/16.
+//  Created by Joshua Escribano on 11/10/16.
 //  Copyright © 2016 jasonify. All rights reserved.
 //
 
 import UIKit
 
-class Quest: NSObject {
+class Milestone: NSObject {
     var objectId: String?
+    var questId: String?
+    var date: Date?
+    var completed: Bool = false
+    var icon: URL?
     var title: String?
     var notes: String?
-    var icon: URL?
-    var tags: [String]?
-    var archived: Bool = false
-    var frequency = 1
     var dictionary: NSDictionary?
     
     init(_ dictionary: NSDictionary) {
-
+        
         self.dictionary = dictionary
-
+        
     }
     
 }
