e9a816214474c26acbb36f4f656f33628d721920
jescriba <escribirajoshua@gmail.com>
2016-11-11 15:51:53 -0800

    Disables or enables creating quest button if title is valid

--- LevelUp/LevelUp/NewQuestViewController.swift
+++ LevelUp/LevelUp/NewQuestViewController.swift
@@ -10,14 +10,61 @@ import UIKit
 
 class NewQuestViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var selectImageView: UIView!
+    @IBOutlet weak var frequencyView: UIView!
+    @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var createQuestButton: UIButton!
+    var disabledButtonColor = UIColor(red:0.17, green:0.40, blue:0.23, alpha:1.0)
+    var enabledButtonColor = UIColor(red:0.38, green:0.90, blue:0.52, alpha:1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let dashColor = UIColor(red:0.60, green:0.61, blue:0.61, alpha:1.0)
+        titleTextField.attributedPlaceholder = NSAttributedString(string: "Music", attributes: [NSForegroundColorAttributeName: dashColor])
+        titleTextField.delegate = self
+        
         createQuestButton.layer.borderWidth = 1.0
         createQuestButton.layer.cornerRadius = 5.0
-        createQuestButton.layer.borderColor = UIColor(red:0.38, green:0.90, blue:0.52, alpha:1.0).cgColor
+        createQuestButton.layer.borderColor = disabledButtonColor.cgColor
+        
+        selectImageView.addDashedBorder(color: dashColor)
+        frequencyView.addDashedBorder(color: dashColor)
+        descriptionTextView.addDashedBorder(color: dashColor)
+        titleTextField.addDashedBorder(color: dashColor)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // TODO
     }
 
+}
+
+extension NewQuestViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let empty = willBeEmpty(textField: textField, replacementString: string)
+        if !empty {
+            createQuestButton.isEnabled = true
+            createQuestButton.layer.borderColor = enabledButtonColor.cgColor
+            createQuestButton.titleLabel?.textColor = enabledButtonColor
+        } else {
+            createQuestButton.isEnabled = false
+            createQuestButton.layer.borderColor = disabledButtonColor.cgColor
+            createQuestButton.titleLabel?.textColor = disabledButtonColor
+        }
+        
+        return true
+    }
+    
+    private func willBeEmpty(textField: UITextField, replacementString string: String) -> Bool {
+        
+        if textField.text?.characters.count == 1 && string.isEmpty {
+            return true
+        }
+        
+        return false
+    }
 }
