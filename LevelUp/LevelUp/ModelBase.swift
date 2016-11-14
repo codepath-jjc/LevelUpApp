04975bf50dc1732f42ae83a33d162bffd1454cbd
jason <bjason@gmail.com>
2016-11-13 22:56:18 -0800

    Quest based out of base model

--- LevelUp/LevelUp/ModelBase.swift
+++ LevelUp/LevelUp/ModelBase.swift
@@ -11,6 +11,55 @@ import Parse
 
 class ModelBase: NSObject {
 
+    
+    
+    class var tableName : String {return "ModelBase"}
+    
+    class var ParseKeys: [String] { return [
+        "title"
+        ]}
+
+        
+    
+    var dictionary = [String: Any]()
+    var _parseObject: PFObject?
+    
+    var objectId: String?
+
+    
+    
+    
+    init(_ _dictionary: [String: Any]) {
+        
+        self.dictionary = _dictionary
+        _parseObject =  PFObject(className: type(of: self).tableName)
+        _parseObject?.setDictionary(_dictionary)
+        
+        super.init()
+        loadFromDictionary(_dictionary)
+    }
+    
+    
+    convenience init(parseObject: PFObject) {
+        
+        // Getting our values from our parse object
+        var _dictionary = [String: Any]()
+        
+        for key in type(of: self).ParseKeys {
+            if let value =  parseObject.object(forKey: key)  {
+                _dictionary[key] = value
+            }
+        }
+        
+        self.init( _dictionary )
+        
+    }
+    
+    
+    func loadFromDictionary(_ dictionary: [String: Any]){
+        
+    }
+    
 }
 
 
--- LevelUp/LevelUp/Quest.swift
+++ LevelUp/LevelUp/Quest.swift
@@ -9,63 +9,29 @@
 import UIKit
 import Parse
 
-let QuestKeys = [
-    "title",
-    "notes"
-]
 
-class Quest: NSObject {
+
+class Quest: ModelBase {
     
-    static let tableName = "Questz020"
-    var objectId: String?
+   override class var tableName : String {return "Quests001"}
+   override  class var ParseKeys: [String] { return [
+        "title"
+        ]}
+    
+    
     var title: String?
     var notes: String?
     var icon: URL?
     var tags: [String]?
     var archived: Bool = false
     var frequency = 1
-    var dictionary = [String: Any]()
-    var _parseObject: PFObject?
-    
-    
-    init(_ _dictionary: [String: Any]) {
-        
-        self.dictionary = _dictionary
-        _parseObject =  PFObject(className: Quest.tableName)
-        _parseObject?.setDictionary(_dictionary)
-        
-        super.init()
-        loadFromDictionary(_dictionary)
-    }
-    
-    
-    convenience init(parseObject: PFObject) {
-        
-        // Getting our values from our parse object
-        var _dictionary = [String: Any]()
-        
-        for key in QuestKeys {            
-            if let value =  parseObject.object(forKey: key)  {
-                print("key", key, "value", value)
-                let name: AnyClass! = object_getClass(value)
-                print(name)
-                _dictionary[key] = value
-            }
-        }
-
-        
-        self.init( _dictionary )
-        
-    }
-    
+  
     // Override this:
-    func loadFromDictionary(_ dictionary: [String: Any]){
+    override func loadFromDictionary(_ dictionary: [String: Any]){
         if let _title =  dictionary["title"] as? String  {
             title  = _title
         }
     }
-    
-    
     
     
     func save(){
