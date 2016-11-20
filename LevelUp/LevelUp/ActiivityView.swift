0d0f84868eb56a20208175d0f59cb304c6e898d4
jason <bjason@gmail.com>
2016-11-19 17:56:47 -0800

    clearing up the background

--- LevelUp/LevelUp/ActiivityView.swift
+++ LevelUp/LevelUp/ActiivityView.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ActiivityView: UIView {
 
-    /*
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //initSubviews()
+    self.backgroundColor = UIColor.clear
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //initSubviews()
+    self.backgroundColor = UIColor.clear
+    }
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-    
-    override func draw(_ rect: CGRect) {
+ 
         
         // Get the Graphics Context
         var path = UIBezierPath(ovalIn: rect)
