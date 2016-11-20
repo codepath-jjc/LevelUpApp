84acb5ad3de0584c6b3d87af6b46b24bc6d271b9
jescriba <escribirajoshua@gmail.com>
2016-11-19 19:25:12 -0800

    Adds tableview to milestone quest selection

--- LevelUp/LevelUp/MilestoneViewController.swift
+++ LevelUp/LevelUp/MilestoneViewController.swift
@@ -15,7 +15,9 @@ class MilestoneViewController: UIViewController {
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var notesTextView: UITextView!
     @IBOutlet weak var chooseImageLabel: UILabel!
-    @IBOutlet weak var questNameLabel: UILabel!
+    @IBOutlet weak var questNameTableView: UITableView!
+    
+    var hasPlaceholder = true
     var quest:Quest? {
         didSet {
             
@@ -32,6 +34,8 @@ class MilestoneViewController: UIViewController {
         doneButton.layer.borderWidth = 1.0
         doneButton.layer.cornerRadius = 5.0
         doneButton.layer.borderColor = UIColor(red:0.38, green:0.90, blue:0.52, alpha:1.0).cgColor
+        
+        notesTextView.delegate = self
     }
     
     @IBAction func onChooseImage(_ sender: UITapGestureRecognizer) {
@@ -47,4 +51,32 @@ class MilestoneViewController: UIViewController {
         navigationDelegate?.page = Page.profile
     }
 
+}
+
+extension MilestoneViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        // Remove placeholder
+        if hasPlaceholder {
+            textView.text = ""
+            hasPlaceholder = false
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        
+        return true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        // Reinsert placeholder
+        if textView.text.isEmpty && !hasPlaceholder {
+            textView.text = "Notes"
+            hasPlaceholder = true
+        }
+    }
 }
