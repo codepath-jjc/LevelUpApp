333da7e6b3f96d729111e4dccfc23d3bff081ea0
jason <bjason@gmail.com>
2016-12-01 03:14:07 -0800

    flare tests

--- LevelUp/LevelUp/TutorialViewController.swift
+++ LevelUp/LevelUp/TutorialViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class TutorialViewController: UIViewController {
 
+    @IBOutlet weak var yConstraint: NSLayoutConstraint!
+    @IBOutlet weak var xConstraint: NSLayoutConstraint!
     @IBOutlet weak var createQuestButton: UIButton!
+    @IBOutlet weak var descLabel: UILabel!
+    
+    @IBOutlet weak var createQuestBtn: UIButton!
+    var snapBehaviour:UISnapBehavior!
+    var animator:UIDynamicAnimator!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +26,19 @@ class TutorialViewController: UIViewController {
         createQuestButton.layer.borderWidth = 1.0
         createQuestButton.layer.cornerRadius = 5.0
         createQuestButton.layer.borderColor = UIColor(red:0.38, green:0.90, blue:0.52, alpha:1.0).cgColor
+        
+        
+        
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        print(xConstraint.constant)
+        descLabel.layoutIfNeeded()
+        descLabel.setNeedsLayout()
+        snapBehaviour = UISnapBehavior(item: descLabel, snapTo: CGPoint(x: xConstraint.constant, y: yConstraint.constant))
+        print(xConstraint.constant)
+
+        snapBehaviour.damping = 0.3
+        animator.addBehavior(snapBehaviour)
     }
 
 }
