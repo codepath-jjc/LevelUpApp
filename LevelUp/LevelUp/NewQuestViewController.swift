e82937df188d50fa2037fa22c88349ee18935869
jescriba <escribirajoshua@gmail.com>
2016-12-04 16:28:13 -0800

    Updates selecting image on new quest page

--- LevelUp/LevelUp/NewQuestViewController.swift
+++ LevelUp/LevelUp/NewQuestViewController.swift
@@ -13,23 +13,15 @@ class NewQuestViewController: UIViewController  {
 
     @IBOutlet weak var frequencySegmentControl: UISegmentedControl!
     @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var selectImageView: UIView!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var createQuestButton: UIButton!
-    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var icon: SelectableImageView!
     
     var navigationDelegate: TabBarViewController?
     var hasPlaceholder = true
     var disabledButtonColor = UIColor(red:0.17, green:0.40, blue:0.23, alpha:1.0)
     var enabledButtonColor = UIColor(red:0.38, green:0.90, blue:0.52, alpha:1.0)
     var frequency = Frequency.daily
-    let imgPicker = UIImagePickerController()
-    
-    var chosenImage: UIImage? {
-        didSet{
-            icon.isHidden = false
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,10 +31,6 @@ class NewQuestViewController: UIViewController  {
         
         descriptionTextView.setNeedsLayout()
         descriptionTextView.layoutIfNeeded()
-        
-        icon.isHidden = true
-        icon.layer.masksToBounds = true
-        icon.layer.cornerRadius = 10
         
         let dashColor = UIColor(red:0.60, green:0.61, blue:0.61, alpha:1.0)
         titleTextField.attributedPlaceholder = NSAttributedString(string: "", attributes: [NSForegroundColorAttributeName: dashColor])
@@ -54,17 +42,13 @@ class NewQuestViewController: UIViewController  {
         createQuestButton.layer.cornerRadius = 5.0
         createQuestButton.layer.borderColor = disabledButtonColor.cgColor
         
-        selectImageView.addDashedBorder(color: dashColor)
-      
         descriptionTextView.addDashedBorder(color: dashColor)
         titleTextField.addDashedBorder(color: dashColor)
 
-        imgPicker.delegate = self
-        imgPicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
-        imgPicker.allowsEditing = false
-
         frequencySegmentControl.addTarget(self, action: #selector(frequencyChanged), for: .valueChanged)
         frequencySegmentControl.tintColor = AppColors.PrimaryAccentColor
+        
+        icon.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -80,10 +64,6 @@ class NewQuestViewController: UIViewController  {
         }
     }
     
-    @IBAction func onImagePickerPress(_ sender: UITapGestureRecognizer) {
-        present(imgPicker, animated: false, completion: nil)
-    }
-    
     @IBAction func onCancelPress(_ sender: Any) {
         if let navigationDelegate = navigationDelegate {
             navigationDelegate.page = Page.profile
@@ -95,7 +75,7 @@ class NewQuestViewController: UIViewController  {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         var dictionary = ["frequency": frequency.rawValue, "title": titleTextField.text ?? "", "notes": descriptionTextView.text, "archived": false] as [String: Any]
-        if let chosenImage = chosenImage {
+        if let chosenImage = icon.image {
             dictionary["image"] = chosenImage
         }
         
@@ -113,25 +93,6 @@ class NewQuestViewController: UIViewController  {
     }
 
 }
-
-
-extension NewQuestViewController:  UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
-
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
-        chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage
-        
-        icon.image = chosenImage
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
-    }
-
-}
-
 
 extension NewQuestViewController: UITextFieldDelegate {
     
