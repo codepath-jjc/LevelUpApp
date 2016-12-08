d2484fc399a9321653c1476874c4bc32f31e6285
jason <bjason@gmail.com>
2016-12-08 12:09:33 -0800

    pet back and forth

--- LevelUp/LevelUp/GameScene.swift
+++ LevelUp/LevelUp/GameScene.swift
@@ -12,6 +12,8 @@ import SpriteKit
 class GameScene: SKScene , SKPhysicsContactDelegate{
     
     
+    var backgroundLayer: SKSpriteNode?
+
     let flare = SKEmitterNode(fileNamed: "Flare")
     
     var playerDirection:CGFloat = 1.0
@@ -22,6 +24,11 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
     
     override func didMove(to view: SKView) {
         // 2
+
+        // Holds eve
+        backgroundLayer = SKSpriteNode();
+        self.addChild(backgroundLayer!);
+
         
      
         backgroundColor = SKColor.white
@@ -29,24 +36,35 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         player.name = "player"
         // 4
-        addChild(player)
-            flare?.name = "playerflare"
+        backgroundLayer?.addChild(player)
+        flare?.name = "playerflare"
         flare?.isHidden = true
         player.addChild(flare!)
-        
+       player.zPosition = 2
         
     }
     
     override func update(_ currentTime: TimeInterval) {
         
+        
         var playerX = player.position.x
         playerX = playerX +  playerDirection * playerSpeed
         print("positionX", playerX)
         player.position = CGPoint(x:  playerX, y: player.position.y)
-        
+ 
+        if(playerX >=  300 || playerX <= 0){ //(backgroundLayer?.frame.size.width)!){
+            playerDirection *= -1.0
+        }
         
 
     }
+    
+    
+    fileprivate func shakeScreen(_ duration:Float = 0.3) {
+        let shakeAction = GameScene.shake(self.backgroundLayer!.position, duration: duration);
+        self.backgroundLayer!.run(shakeAction);
+    }
+    
 
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -54,10 +72,11 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         
         let location = touches.first!.location(in: self)
         
-        self.enumerateChildNodes(withName: "player") { node, stop in
+        backgroundLayer?.enumerateChildNodes(withName: "player") { node, stop in
             
             if node.contains(location) && node.isHidden == false {
                 print("TOUCH PET")
+                self.shakeScreen()
              //   node.isHidden = true
                 self.flare?.isHidden = false
                // stop.memory = true
@@ -65,6 +84,23 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         }
 
     }
+    
+    
+    // Shake function borrowed from - http://stackoverflow.com/questions/20889860/a-camera-shake-effect-for-spritekit
+    class func shake(_ initialPosition:CGPoint, duration:Float, amplitudeX:Int = 12, amplitudeY:Int = 3) -> SKAction {
+        let startingX = initialPosition.x
+        let startingY = initialPosition.y
+        let numberOfShakes = duration / 0.015
+        var actionsArray:[SKAction] = []
+        for index in 1...Int(numberOfShakes) {
+            let newXPos = startingX + CGFloat(arc4random_uniform(UInt32(amplitudeX))) - CGFloat(amplitudeX / 2)
+            let newYPos = startingY + CGFloat(arc4random_uniform(UInt32(amplitudeY))) - CGFloat(amplitudeY / 2)
+            actionsArray.append(SKAction.move(to: CGPoint(x: newXPos, y: newYPos), duration: 0.015))
+        }
+        actionsArray.append(SKAction.move(to: initialPosition, duration: 0.015))
+        return SKAction.sequence(actionsArray)
+    }
+    
     
     
 }
